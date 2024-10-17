f33fa694048a15a8238e5fa0dd8eafa9a762319a
nicolascenzano <111360446+nicolascenzano@users.noreply.github.com>
2024-10-17 16:34:33 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Clase 17 de Octubre 2024</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
